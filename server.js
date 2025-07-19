c270b464cc9725242612a9cac0ad283a09c71f5f
Ricardo Cabrera <114771338+Ricardo0609@users.noreply.github.com>
2025-07-18 18:00:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -59,3 +59,11 @@ app.get('/test-db', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
+
+
+
+
+const path = require('path');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
